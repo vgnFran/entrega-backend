2b9c6a40f9c697a4f35f83573e9a701f2f8d70c2
vgnFran <francohoyos2112@gmail.com>
2023-05-14 17:48:36 -0300

    manager FS

--- src/managers/productManagerFS.js
+++ src/managers/productManagerFS.js
@@ -7,38 +7,71 @@ class ProductsManager{
         this.products=[]
     }
 
+    
 
-    getProducts= ()=>{
+    fsProducts= ()=>{
         return JSON.parse(fs.readFileSync(this.path,"utf-8"))
     }
-    
+
+
     getProductsById=(id)=>{
-        const filtered= this.getProducts().find(prod=>{
+        const filtered= this.fsProducts().find(prod=>{
             return prod.id == id
         })
-        if(filtered){
-            return filtered
-        }else{
-            return `product ${id} does not exist in the database`
+        return filtered
+     }
+
+
+    createProduct=(data)=>{
+
+        const newId= prod1.fsProducts().pop().id+1
+
+        const newProduct={
+            id:newId,
+            title:data.title,
+            description:data.description,
+            code:data.code,
+            price:data.price,
+            status:true,
+            stock:data.stock,
+            category:data.category,
+            thumbnail:data.thumbnail
         }
+        return newProduct
+
     }
 
-    updatePoducts=(id,data)=>{
-        const update= this.getProductsById(id)
-        if(update){
+
+    updatePoduct=(update,data)=>{
+
             const updatedProduct= {...update,...data}
+            const notUpdate= this.fsProducts().filter(prod=>{
+                return prod.id !=  update.id
+            })
+             
+            notUpdate.push(updatedProduct)
+            fs.writeFileSync("./src/products.json",JSON.stringify(notUpdate))
             return updatedProduct
-        }else{
-            return 
-        }
        
     }
 
-    
+    deleteProduct=(deleted)=>{
+
+        if(deleted){
+            const withoutRemoved= productsParse.filter(prod=>{
+                return prod.id != deleted.id
+            })
+            fs.writeFileSync("./src/products.json",JSON.stringify(withoutRemoved))
+        }
+    }
+
+
 }
 
+export default ProductsManager
 
 
 
-const prod1= new ProductsManager("./src/products.json")
-console.log(prod1.updatePoducts(11,{title:"pc vieja"}))
+
+
+
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import fs from "fs"
-
+import ProductsManager from "../managers/productManagerFS.js"
 
 
 const productsRoutes=(io)=>{
@@ -8,29 +8,20 @@ const productsRoutes=(io)=>{
     const router= Router()
     const products=[]
     const productsParse=JSON.parse(fs.readFileSync("./src/products.json"))
+    const manager=new ProductsManager("./src/products.json")
 
-    router.get("/products", (req,res)=>{
-        if(req.query.limit){
-            const limitedProducts= productsParse.filter((prod)=>{
-                return prod.id <= req.query.limit
-            })
-            res.status(200).send(limitedProducts)
-        }else{
-            res.status(200).send(productsParse)
-        }
+
+    router.get("/products",(req,res)=>{
+        res.status(200).send(manager.fsProducts())
     })
-    
+   
     
     router.get("/products/:id",(req,res)=>{
         
-        const searchId= productsParse.find((prod)=>{
-            return prod.id == req.params.id
-        })
-    
-        if (searchId){
-            res.status(200).send(searchId)
-        }else {
-            res.status(404).send ({
+        if(manager.getProductsById(req.params.id)){
+            res.status(200).send(manager.getProductsById(req.params.id))
+        }else{
+            res.status(404).send({
                 id:req.params.id,
                 product: `product ${req.params.id} does not exist in the database`
             })
@@ -40,61 +31,41 @@ const productsRoutes=(io)=>{
     
     
     
-    
     router.post("/products", (req,res)=>{
     
-        const newProduct={
-            id:JSON.parse(fs.readFileSync("./products.json")).length+1,
-            title:req.body.title,
-            description:req.body.description,
-            code:req.body.code,
-            price:parseInt(req.body.price),
-            status:true,
-            stock:parseInt(req.body.stock),
-            category:req.body.category,
-            thumbnail:[req.body.thumbnail]
-        }
-    
-        const repited= productsParse.find(prod =>{
+            
+        // verificamos si todos los imputs estan completos, si hay alguno incompleto la validacion sera true
+        const values = Object.values(manager.createProduct(req.body))
+        const completeImputs= values.map(val=>{
+            return val== null
+        })
+      
+        // verificamos si el codigo del nuevo producto ya existe
+        const repited= manager.fsProducts().find(prod=>{
             return prod.code == req.body.code
         })
-    
-        if(repited){
-            res.status(404).send("The product already exists in the database")
-        }else if(req.body.title != undefined && req.body.description != undefined && req.body.code != undefined && req.body.price != undefined && req.body.stock != null && req.body.category != undefined){
-            productsParse.push(newProduct)
-            res.status(200).send(productsParse)
-            fs.writeFileSync("./src/products.json",JSON.stringify(productsParse))
-    
-        }else {
+
+
+        if (completeImputs.includes(true)){
             res.status(404).send("Incomplete field")
+        }else if (repited){
+            res.status(404).send("The product alredy exists in the database")
+        }else{
+            res.status(200).send(manager.createProduct(req.body))
         }
-            
+        
     })
     
     
     router.put("/products/:id",(req,res)=>{
     
-        const updated=productsParse.find(prod =>{
-            return prod.id == req.params.id
-        })
-    
-    
-        if(updated){
-    
-            const updatedProduct= {...updated,...req.body}
-    
-            const notUpdate= productsParse.filter(prod=>{
-                return prod.id != req.params.id
-            })    
-            notUpdate.push(updatedProduct)
-            
-            fs.writeFileSync("./src/products.json",JSON.stringify(notUpdate))
-            res.status(200).send(updatedProduct)
-    
-        } else{
+        const update=manager.getProductsById(req.params.id)
+        if (update){            
+            res.status(200).send(manager.updatePoduct(update,req.body))
+        }else{
             res.status(404).send(`the product ${req.params.id} does not exist`)
         }
+        
         
     })
     
@@ -102,18 +73,16 @@ const productsRoutes=(io)=>{
     
     router.delete("/products/:id",(req,res)=>{
     
-        const deleted= productsParse.find(prod=>{
+
+        const deleted= manager.fsProducts().find(prod=>{
             return prod.id == req.params.id
         }) 
-    
+
         if(deleted){
-            const withoutRemoved= productsParse.filter(prod=>{
-                return prod.id != deleted.id
-            })
-            fs.writeFileSync("./src/products.json",JSON.stringify(withoutRemoved))
-            res.status(200).send(withoutRemoved)
-        } else{
-            res.status(404).send(`the product with the id:${req.params.id} does not exist in the database`)
+            manager.deleteProduct(deleted)
+            res.status(200).send("Product deleted")
+        }else{
+            res.status(404).send(`The product with the id:${req.params.id} does not exist in the database`)
         }
     
     })
