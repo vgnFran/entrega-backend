d5b4787dbe7cebfe25daf04501cf23a56874cbfe
vgnFran <francohoyos2112@gmail.com>
2023-06-01 18:06:39 -0300

    hash

--- utils.js
+++ utils.js
@@ -1,6 +1,16 @@
 import * as url from 'url';
+import bcrypt from "bcrypt";
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
-export { __filename, __dirname };
+const hashing= (pass)=>{
+    return bcrypt.hashSync(pass, bcrypt.genSaltSync(10))
+}
+
+const compareHash= (user,pass)=>{
+    return bcrypt.compareSync(pass,user.password)
+}
+
+
+export { __filename, __dirname, hashing, compareHash };
